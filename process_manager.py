aa7ff02c751d365470fbc6658254020fba6361e2
Grier Phillips <GrierPhillips@gmail.com>
2017-03-07 09:42:48 -0700

    Updated process_manager.py command method and added _copy_file private method for copyin event.log files and summary stat files.

--- process_manager.py
+++ process_manager.py
@@ -4,6 +4,8 @@ the ABM->ISAM->DTA pipeline.
 '''
 
 import subprocess
+from os import makedirs, path
+from shutil import copy2
 from ab_dst2 import AB_DST2
 
 
@@ -12,21 +14,29 @@ class ProcessManager(object):
     Class for managing the processes involved with iterating though ISAM-DTA
     loops.
     '''
-    def __init__(self, isam_properties, dta_properties):
-        self.isam_props = isam_properties
+    def __init__(self, dta_properties):
         self.dta_props = dta_properties
 
-    def call_command(self, command):
+    def call_command(self, script, iteration):
         '''
         Call a shell command and wait for it to execute.
         Input:
             command: String. A command that executes on the command line.
-
+            args: Iterator. A collection of command line arguments.
         Example:
-            call_command('runISAM.cmd', check=True)
-        This will execute runISAM.cmd and wait for it to exit.
+            call_command('runISAM.cmd', (0))
+        This will execute runISAM.cmd with the argument 0 and wait for the
+        process to exit.
         '''
-        subprocess.run(command, check=True)
+        command = script + ' ' + str(iteration)
+        result = subprocess.run(
+            command,
+            check=True,
+            stderr=subprocess.PIPE,
+            universal_newlines=True)
+        event_file = 'iter{2}\\event_{0}{1}{2}.log'.format(
+            command[3].lower(), command[4:-4], iteration)
+        self._copy_log_file('event.log', event_file)
 
     def run_dynust(self):
         '''
@@ -35,3 +45,10 @@ class ProcessManager(object):
         '''
         ab_dst = AB_DST2(self.dta_props)
         ab_dst.run()
+
+    @staticmethod
+    def _copy_log_file(source, destination):
+        directory = path.dirname(destination)
+        if not path.exists(directory):
+            makedirs(directory)
+        copy2(source, destination)
