f0f6a1166a3df259e8c18e78e3a93fabb8571ac1
Grier Phillips <GrierPhillips@gmail.com>
2017-03-08 13:53:22 -0700

    Completed implementation of DSTPropGen class. Made small modifications to _set_values and _set_path in BasePropGen class.

--- property_generator.py
+++ property_generator.py
@@ -1,7 +1,15 @@
+'''
+property_generator.py: This module contains the DSTPropGen and IsamPropGen
+classes. These classes are used for generating property files for the AB_DST
+class and for use with the runIsam.cmd batch file.
+'''
 import abc
 
 
 class BasePropGen(object):
+    '''
+    Abstract base class for defining property generator classes.
+    '''
     __metaclass__ = abc.ABCMeta
 
     def __init__(self, template, parameters):
@@ -49,12 +57,13 @@ class BasePropGen(object):
             outer (string): The current outer loop iteration number.
             inner (string): The current inner loop iteration number.
         '''
+        basepath = self.user_params['basepath']
         for key, value in self.user_params.items():
             if key in self.root_path_keys or key in self.loop_path_keys:
-                self._set_path(key, outer, inner)
+                self._set_path(key, outer, inner, basepath)
             self.params[key] = value
 
-    def _set_path(self, key, outer, inner):
+    def _set_path(self, key, outer, inner, basepath):
         '''
         Set the proper paths for the values in the property file that need
         them.
@@ -62,23 +71,19 @@ class BasePropGen(object):
         Args:
             outer (string): The current outer loop iteration number.
             inner (string): The current inner loop iteration number.
+            basepath (string): The value entered by user for BASEPATH.
         '''
-        if key in self.root_path_keys:
-            self.set_root_paths()
-        else:
-            self.set_loop_paths(outer, inner)
-            root = self.user_params['root'] + '\\'
-            path = 'outer{}\\inner{}\\'.format(outer, inner)
-            path = root + path + self.loop_path_keys[key].replace('#', inner)
-            self.params[key] = path
+        self.set_root_paths(key, basepath)
+        if key in self.loop_path_keys:
+            self.set_loop_paths(key, outer, inner)
 
     @abc.abstractmethod
-    def set_root_paths(self):
+    def set_root_paths(self, key, basepath):
         '''Method for setting the BASEPATH segment of values that do not
         contain loop folders in their path.'''
 
     @abc.abstractmethod
-    def set_loop_paths(self, outer, inner):
+    def set_loop_paths(self, key, outer, inner):
         '''Method for setting the BASEPATH and loop directory segments of
         values that do contain loop folders in their path.'''
 
@@ -100,6 +105,37 @@ class BasePropGen(object):
             file_obj.writelines(lines)
 
 
-class DSTPropertyGenerator(BasePropGen):
+class DSTPropGen(BasePropGen):
+    '''
+    Class for generating property files for use by AB_DST objects.
+    '''
     def __init__(self, template, parameters):
-        super(DSTPropertyGenerator, self).__init__(template, parameters)
+        super(DSTPropGen, self).__init__(template, parameters)
+
+    def set_root_paths(self, key, basepath):
+        '''
+        Set the BASEPATH value in the parameter for key to the value entered
+        by the user.
+
+        Args:
+            key (string): The key for the parameter to be set.
+            basepath (string): The value entered by user for BASEPATH.
+        '''
+        self.params[key] = self.params[key].replace('BASEPATH', basepath)
+
+    def set_loop_paths(self, key, outer, inner):
+        '''
+        Set the LOOP_PAIR value in the parameter for key to the value of the
+        current outer loop and inner loop.
+
+        Args:
+            key (string): The key for the parameter to be set.
+            outer (string): The current outer loop iteration number.
+            inner (string): The current inner loop iteration number.
+        '''
+        if 'LOOP_PAIR' in self.params[key]:
+            loop_pair = 'outer{}\\inner{}'.format(outer, inner)
+            self.params[key] = self.params[key].replace('LOOP_PAIR', loop_pair)
+        else:
+            outer_loop = 'outer{}'.format(outer)
+            self.params[key] = self.params[key].replace('OUTER', outer_loop)
